c254a05b4c14ea56db9ad5413f4555a480d874cc
Andrew Murray <andrew.2013.murray@gmail.com>
2014-04-25 01:21:01 +0100

    Timer stub.

---
+++ src/instruments/scoped_timer.hpp
@@ -0,0 +1,55 @@
+#include <chrono>
+#include <thread>
+namespace metrics{
+	namespace instruments {
+		#define hint_likely(x)	__builtin_expect(!!(x), 1)
+
+		template<typename ClockType = std::chrono::high_resolution_clock>
+		class scoped_timer{
+		public:
+
+			typedef typename ClockType::time_point time_point;
+			typedef typename ClockType::duration duration;
+
+			template<typename... ArgTypes >
+			scoped_timer(std::function<void(const duration&)> recorder
+							= std::function<void(const duration&)>(
+								[&](const duration& dur){
+									std::cout << dur << std::endl;
+								})
+						) __attribute__((always_inline))
+			: m_clock()
+			, m_running(true)
+			, m_record(recorder)
+			, m_start(m_clock.now())
+			{
+
+			}
+
+
+			void start() __attribute__((always_inline)){
+				m_start = m_clock.now();
+				m_running = true;
+			}
+
+			void stop() __attribute__((always_inline)){
+				m_time = std::chrono::duration_cast<duration>(m_clock.now() - m_start);
+			}
+
+			~scoped_timer() __attribute__((always_inline)){
+				if(hint_likely(m_running)){
+					stop();
+				}
+				m_record(m_time);
+				//std::cout << m_time.count() << std::endl;
+			}
+
+			ClockType m_clock;
+			duration m_time;
+			std::function<void(const duration&)> m_record;
+			bool m_running;
+			// important to initialize this last !
+			time_point m_start;
+		};
+	}
+}
--- test/histogram_test.cpp
+++ test/histogram_test.cpp
@@ -1,6 +1,11 @@
 #include <boost/test/unit_test.hpp>
 #include "instruments/histogram.hpp"
+#include "instruments/timer.hpp"
+#include <chrono>
 
 BOOST_AUTO_TEST_CASE(histogram_test){
 	metrics::instruments::histogram h;
+	using metrics::instruments::scoped_timer;
+	scoped_timer<>::duration local_duration;
+	scoped_timer<>([&](const scoped_timer<>::duration& time){local_duration = time;});
 }
